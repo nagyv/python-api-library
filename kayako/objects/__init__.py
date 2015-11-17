356970784906c7cec772eca2333146dd04ac7888
Viktor Nagy <viktor.nagy@gmail.com>
2015-11-17 11:54:56 +0100

    kayako_api refactor / again

---
+++ kayako/objects/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+# Copyright (c) 2011, Evan Leis
+#
+# Distributed under the terms of the Lesser GNU General Public License (LGPL)
+#-----------------------------------------------------------------------------
+from .department import *
+from .staff import *
+from .ticket import *
+from .user import *
+from .knowledgebase import *
+from .custom_field import *
+from .news import *
+from .troubleshooter import *
